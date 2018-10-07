2252d6a3fc6961e19aa34ff8dde8bdd6b6d8161e
su.junjie.zh <su.junjie.zh@gmail.com>
2018-10-08 00:01:39 +0800

    edit evaluation.sh

--- src/evaluation.sh
+++ src/evaluation.sh
@@ -4,9 +4,9 @@ python main.py \
  ./data_preprocess/train_train_list.txt  \
  ./data_preprocess/test_list.txt \
  --gpu=2 \
- --save_dir=../output/resnet50 \
- --arch=resnet50 \
- --batch-size=32 \
- --resume=../output/resnet50/resnet50_best.pth.tar \
+ --save_dir=../output/bninception_trimmed \
+ --arch=bninception_trimmed \
+ --batch-size=4 \
+ --resume=../output/bninception_trimmed/bninception_trimmed_best.pth.tar \
  --evaluate
 
