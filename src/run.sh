6c81513ef70c013e85c3042830f69dc61c2c86ce
su.junjie.zh <su.junjie.zh@gmail.com>
2018-10-10 15:45:36 +0800

    modify running code

--- src/run.sh
+++ src/run.sh
@@ -5,7 +5,7 @@
 python main.py \
  ./data_preprocess/train_train_list.txt  \
  ./data_preprocess/train_valid_list.txt \
- --gpu=2 \
+ --gpu=1 \
  --save_dir=../output/bninception \
  --arch=bninception \
  --batch-size=128
